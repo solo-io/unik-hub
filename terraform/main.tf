3f51f35697992c648991192ac3a66c8bf58afa80
Scott Weiss <scott.weiss@emc.com>
2016-08-22 10:37:17 -0400

    create terraform plan
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2d39803a"
+  instance_type = "t2.small"
+}
